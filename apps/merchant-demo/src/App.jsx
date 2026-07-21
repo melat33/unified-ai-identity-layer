701b4ad9b6168c4bc67f433e20716672b22eabe2
Melat Tewachew <melattewachew@gmail.com>
2026-07-21 11:29:01 +0300

    feat(merchant): configure BrowserRouter with / landing and /callback routes

--- apps/merchant-demo/src/App.jsx
+++ apps/merchant-demo/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Landing  from "./pages/Landing"
 import Callback from "./pages/Callback"
 
@@ -6,8 +6,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"          element={<Landing />}  />
+        <Route path="/"          element={<Landing />} />
         <Route path="/callback"  element={<Callback />} />
+        <Route path="*"          element={<Landing />} />
       </Routes>
     </BrowserRouter>
   )
